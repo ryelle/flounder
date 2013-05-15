cc6077334cf67d1458af16a21a2ec6fb72544667
Kelly Dwan <kelly.dwan@gmail.com>
2013-05-15 12:38:13 -0400

    Remove footer (sidebar meant for this type of info)

--- footer.php
+++ footer.php
@@ -8,14 +8,6 @@
  */
 ?>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'flounder_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'flounder' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'flounder' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'flounder' ), 'Flounder', '<a href="http://underscores.me/" rel="designer">Kelly Dwan & Mel Choyce</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
