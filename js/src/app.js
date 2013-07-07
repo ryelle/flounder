95babdfabf3a1a67245b899cc563e0f734827dbe
Kelly Dwan <kelly.dwan@gmail.com>
2013-07-07 19:53:06 -0400

    Include jetpack's subscription checkboxes in the dropdown fields.

--- js/src/app.js
+++ js/src/app.js
@@ -2,9 +2,10 @@ jQuery( document ).ready( function( $ ) {
 
 	var $fields = $( document.getElementById( 'comment-info-fields' ) ),
 		$submit = $( document.getElementById( 'submit' ) ),
+		$jetpack = $( document.getElementById( 'jetpack-subscribe' ) ),
 		$textarea = $( document.getElementById( 'comment' ) );
 
-	$fields.append( $submit ).hide();
+	$fields.append( $submit ).append( $jetpack ).hide();
 	
 	$textarea.on( 'focus', function(){
 		$fields.insertAfter( this ).slideDown();
