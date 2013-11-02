3ba4c1727588aca3e7524fabe2b2377957eee7e2
Kelly Dwan <kelly.dwan@gmail.com>
2013-11-02 15:54:47 -0400

    Remove back-compat for 3.4, we only need to worry about 3.5+

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -36,43 +36,9 @@ function flounder_custom_header_setup() {
 
 	$args = apply_filters( 'flounder_custom_header_args', $args );
 
-	if ( function_exists( 'wp_get_theme' ) ) {
-		add_theme_support( 'custom-header', $args );
-	} else {
-		// Compat: Versions of WordPress prior to 3.4.
-		define( 'HEADER_TEXTCOLOR',    $args['default-text-color'] );
-		define( 'HEADER_IMAGE',        $args['default-image'] );
-		define( 'HEADER_IMAGE_WIDTH',  $args['width'] );
-		define( 'HEADER_IMAGE_HEIGHT', $args['height'] );
-		add_custom_image_header( $args['wp-head-callback'], $args['admin-head-callback'], $args['admin-preview-callback'] );
-	}
+	add_theme_support( 'custom-header', $args );
 }
 add_action( 'after_setup_theme', 'flounder_custom_header_setup' );
-
-/**
- * Shiv for get_custom_header().
- *
- * get_custom_header() was introduced to WordPress
- * in version 3.4. To provide backward compatibility
- * with previous versions, we will define our own version
- * of this function.
- *
- * @todo Remove this function when WordPress 3.6 is released.
- * @return stdClass All properties represent attributes of the curent header image.
- *
- * @package Flounder
- */
-
-if ( ! function_exists( 'get_custom_header' ) ) {
-	function get_custom_header() {
-		return (object) array(
-			'url'           => get_header_image(),
-			'thumbnail_url' => get_header_image(),
-			'width'         => HEADER_IMAGE_WIDTH,
-			'height'        => HEADER_IMAGE_HEIGHT,
-		);
-	}
-}
 
 if ( ! function_exists( 'flounder_header_style' ) ) :
 /**
