6668b0d9b30e04ec55de671dd915b6cae991db11
Kelly Dwan <kelly.dwan@gmail.com>
2013-11-02 17:18:55 -0400

    Remove default footer if infinite scroll is active

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -9,11 +9,13 @@
 /**
  * Add theme support for Infinite Scroll.
  * See: http://jetpack.me/support/infinite-scroll/
+ * Use `__return_false` as the `footer_callback` so we don't show a footer.
  */
 function flounder_infinite_scroll_setup() {
 	add_theme_support( 'infinite-scroll', array(
-		'container' => 'content',
-		'footer'    => 'page',
+		'container'       => 'content',
+		'footer'          => 'page',
+		'footer_callback' => '__return_false',
 	) );
 }
 add_action( 'after_setup_theme', 'flounder_infinite_scroll_setup' );
