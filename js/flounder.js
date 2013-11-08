6fe8c3a63ba88c4e6d65a20f153fe899cbe7920f
Kelly Dwan <kelly.dwan@gmail.com>
2013-11-08 00:17:54 -0500

    Fix up checks- only attach click events if the object exists.

--- js/flounder.js
+++ js/flounder.js
@@ -21,22 +21,26 @@ jQuery( document ).ready( function( $ ) {
 	var container  = document.getElementById( 'page' ),
 	    navIcon    = document.getElementById( 'toggle-nav' ),
 	    widgetIcon = document.getElementById( 'toggle-widgets' );
-
-	// Bail if we don't see a container or nav icon
-	if ( undefined == container || undefined == navIcon )
-		return false;
+			console.log(  );
+	// Bail if we don't see a container
+	if ( undefined == container )
+		return;
 
 	// Display/hide navigation
-	navIcon.onclick = function() {
-		$( document.body ).removeClass( 'show-widgets' );
-		$( document.body ).toggleClass( 'show-nav' );
-	};
+	if ( undefined != navIcon ) {
+		$( navIcon ).on( 'click', function() {
+			$( document.body ).removeClass( 'show-widgets' );
+			$( document.body ).toggleClass( 'show-nav' );
+		});
+	}
 	
 	// Display/hide navigation
-	widgetIcon.onclick = function() {
-		$( document.body ).removeClass( 'show-nav' );
-		$( document.body ).toggleClass( 'show-widgets' );
-	};
+	if ( undefined != widgetIcon ) {
+		$( widgetIcon ).on( 'click', function() {
+			$( document.body ).removeClass( 'show-nav' );
+			$( document.body ).toggleClass( 'show-widgets' );
+		});
+	}
 } )( jQuery );
 /**
  * Makes "skip to content" link work correctly in IE9 and Chrome for better
--- js/src/menu-wiget.js
+++ js/src/menu-wiget.js
@@ -5,20 +5,24 @@
 	var container  = document.getElementById( 'page' ),
 	    navIcon    = document.getElementById( 'toggle-nav' ),
 	    widgetIcon = document.getElementById( 'toggle-widgets' );
-
-	// Bail if we don't see a container or nav icon
-	if ( undefined == container || undefined == navIcon )
-		return false;
+			console.log(  );
+	// Bail if we don't see a container
+	if ( undefined == container )
+		return;
 
 	// Display/hide navigation
-	navIcon.onclick = function() {
-		$( document.body ).removeClass( 'show-widgets' );
-		$( document.body ).toggleClass( 'show-nav' );
-	};
+	if ( undefined != navIcon ) {
+		$( navIcon ).on( 'click', function() {
+			$( document.body ).removeClass( 'show-widgets' );
+			$( document.body ).toggleClass( 'show-nav' );
+		});
+	}
 	
 	// Display/hide navigation
-	widgetIcon.onclick = function() {
-		$( document.body ).removeClass( 'show-nav' );
-		$( document.body ).toggleClass( 'show-widgets' );
-	};
+	if ( undefined != widgetIcon ) {
+		$( widgetIcon ).on( 'click', function() {
+			$( document.body ).removeClass( 'show-nav' );
+			$( document.body ).toggleClass( 'show-widgets' );
+		});
+	}
 } )( jQuery );
