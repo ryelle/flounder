3e5095cae361085c6eaaed9b44269e519528044b
Kelly Dwan <kelly.dwan@gmail.com>
2014-04-03 12:16:16 -0400

    Wrap entire image circle in link, so that link still exists when the header text is hidden. Fixes #34

--- header.php
+++ header.php
@@ -34,9 +34,11 @@
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		</div>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+			<div class="site-branding">
+				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+			</div>
+		</a>
 
 		<nav id="site-navigation" class="navigation-main" role="navigation">
 			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flounder' ); ?>"><?php _e( 'Skip to content', 'flounder' ); ?></a></div>
